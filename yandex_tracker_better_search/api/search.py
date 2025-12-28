55dd06aa38efc35316e993ddb19a3cddcab90449
Alexander Nalobin <m_troc@mail.ru>
2025-12-28 18:39:27 +0300

    feat: switch from python to go

--- yandex_tracker_better_search/api/search.py
+++
@@ -1,9 +0,0 @@
-from fastapi import APIRouter
-
-router = APIRouter(prefix="", tags=["search"])
-
-
-@router.get("/")
-async def search(search_request: str, additional: str | None = None):
-    task_id = search_request
-    return {"task_id": additional, "status": task_id}
